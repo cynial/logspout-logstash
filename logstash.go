2c9055495bb4f44a5ead6a483c11201262b6d7ed
cynial <huanglve@gmail.com>
2016-04-14 11:08:13 +0000

    change json field name

---
+++ logstash.go
@@ -0,0 +1,73 @@
+package logstash
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net"
+	"os"
+
+	"github.com/gliderlabs/logspout/router"
+)
+
+func init() {
+	router.AdapterFactories.Register(NewLogstashAdapter, "logstash")
+}
+
+// LogstashAdapter is an adapter that streams TCP JSON to Logstash.
+type LogstashAdapter struct {
+	conn  net.Conn
+	route *router.Route
+}
+
+// NewLogstashAdapter creates a LogstashAdapter with TCP as the default transport.
+func NewLogstashAdapter(route *router.Route) (router.LogAdapter, error) {
+	transport, found := router.AdapterTransports.Lookup(route.AdapterTransport("tcp"))
+	if !found {
+		return nil, errors.New("unable to find adapter: " + route.Adapter)
+	}
+
+	conn, err := transport.Dial(route.Address, route.Options)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LogstashAdapter{
+		route: route,
+		conn:  conn,
+	}, nil
+}
+
+// Stream implements the router.LogAdapter interface.
+func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
+	for m := range logstream {
+		msg := LogstashMessage{
+			Message:  m.Data,
+			Name:     m.Container.Name,
+			ID:       m.Container.ID,
+			Image:    m.Container.Config.Image,
+			Hostname: m.Container.Config.Hostname,
+			Labels:   m.Container.Config.Labels,
+		}
+		js, err := json.Marshal(msg)
+		if err != nil {
+			log.Println("logstash:", err)
+			continue
+		}
+		_, err = a.conn.Write([]byte(string(js) + "\n"))
+		if err != nil {
+			log.Println("fatal logstash:", err)
+			os.Exit(3)
+		}
+	}
+}
+
+// LogstashMessage is a simple JSON input to Logstash.
+type LogstashMessage struct {
+	Message  string            `json:"message"`
+	Name     string            `json:"docker_name"`
+	ID       string            `json:"docker_id"`
+	Image    string            `json:"docker_image"`
+	Hostname string            `json:"docker_hostname"`
+	Labels   map[string]string `json:"docker_labels,omitempty"`
+}
